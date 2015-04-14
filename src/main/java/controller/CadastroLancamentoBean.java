da5013d3c56f70c8c244dc22dbe2492c0a7bd88b
welinton <welinton.b.borges@gmail.com>
2015-04-14 13:19:47 -0300

    cadastroLancamentosBean

--- src/main/java/controller/CadastroLancamentoBean.java
+++ src/main/java/controller/CadastroLancamentoBean.java
@@ -1,5 +1,75 @@
 package controller;
 
-public class CadastroLancamentoBean {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import model.Lancamento;
+import model.Pessoa;
+import model.TipoLancamento;
+import repository.CadastroLancamentos;
+import repository.Lancamentos;
+import repository.NegocioException;
+import repository.Pessoas;
+import util.JpaUtil;
+
+public class CadastroLancamentoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Lancamento lancamento = new Lancamento();
+	private List<Pessoa> todasPessoas;
+
+	public void prepararCadastro() {
+		EntityManager manager = JpaUtil.getEntityManager();
+		try {
+			Pessoas pessoas = new Pessoas(manager);
+			this.todasPessoas = pessoas.todas();
+		} finally {
+			manager.close();
+		}
+	}
+
+	public void salvar() {
+		EntityManager manager = JpaUtil.getEntityManager();
+		EntityTransaction trx = manager.getTransaction();
+		FacesContext context = FacesContext.getCurrentInstance();
+		try {
+			trx.begin();
+			CadastroLancamentos cadastro = new CadastroLancamentos(
+					new Lancamentos(manager));
+			cadastro.salvar(this.lancamento);
+			this.lancamento = new Lancamento();
+			context.addMessage(null, new FacesMessage(
+					"Lançamento salvo com sucesso!"));
+			trx.commit();
+		} catch (NegocioException e) {
+			trx.rollback();
+			FacesMessage mensagem = new FacesMessage(e.getMessage());
+			mensagem.setSeverity(FacesMessage.SEVERITY_ERROR);
+			context.addMessage(null, mensagem);
+		} finally {
+			manager.close();
+		}
+	}
+
+	public List<Pessoa> getTodasPessoas() {
+		return this.todasPessoas;
+	}
+
+	public TipoLancamento[] getTiposLancamentos() {
+		return TipoLancamento.values();
+	}
+
+	public Lancamento getLancamento() {
+		return lancamento;
+	}
+
+	public void setLancamento(Lancamento lancamento) {
+		this.lancamento = lancamento;
+	}
 
 }
